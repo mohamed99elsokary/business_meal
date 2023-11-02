2923bc8ce86b1ca5a4ee43dd6e3050319f018a3a
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-11-03 00:46:59 +0200

    fix address update endpoint

--- business_meal/userapp/serializers.py
+++ business_meal/userapp/serializers.py
@@ -114,6 +114,11 @@ class AddressSerializer(serializers.ModelSerializer):
         validated_data["location"] = f"SRID=4326;POINT ({location[0]} {location[1]})"
         return super().create(validated_data)
 
+    def update(self, instance, validated_data):
+        location = validated_data["location"]
+        validated_data["location"] = f"SRID=4326;POINT ({location[0]} {location[1]})"
+        return super().update(instance, validated_data)
+
 
 class VerifySerializer(UserToken, serializers.Serializer):
     phone = serializers.CharField(write_only=True, default="01111155856")
