43c83d6d0a5a3f4682fc40afdd710971c914b9a8
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-11-10 17:41:00 +0200

    add hall and option to hall options list display

--- business_meal/hotel_app/admin.py
+++ business_meal/hotel_app/admin.py
@@ -51,6 +51,7 @@ class HallImagesAdmin(ModelAdmin):
 @admin.register(models.HallOptions)
 class HallOptionsAdmin(ModelAdmin, TranslationAdmin):
     "Admin View for HallOptions"
+    list_display = ("hall", "option")
 
 
 @admin.register(models.HallAvailableTime)
