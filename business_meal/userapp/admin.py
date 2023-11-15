80d63ad3782ca23aad3408237b8189ca2e3875c8
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-11-15 04:43:11 +0200

    add user type filter in user admin

--- business_meal/userapp/admin.py
+++ business_meal/userapp/admin.py
@@ -1,6 +1,5 @@
 from allauth.account.models import EmailAddress
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.gis.db import models as db_models
 from django.utils.translation import gettext_lazy as _
 from mapwidgets.widgets import GooglePointFieldWidget
@@ -47,6 +46,7 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups", "user_type")
 
     @display(header=True)
     def user(self, obj):
