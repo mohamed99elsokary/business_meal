9c7fa7a4f2105225efd38a74aa1a518c52def3d4
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-11-25 21:28:37 +0200

    enhance sending sms senario

--- business_meal/userapp/model_mixins.py
+++ business_meal/userapp/model_mixins.py
@@ -23,19 +23,24 @@ class UserMixin:
             user = cls.objects.create(
                 phone=validated_data["phone"], email=f"{latest_user.id+1}@email.com"
             )
-        user.send_otp()
+        is_send_msg = SiteConfiguration.is_send_msg
+
+        user.send_otp(is_send_msg=is_send_msg)
         return user
 
-    def send_otp(self, phone=None):
-        if SiteConfiguration.is_send_msg:
+    def send_otp(self, phone=None, is_send_msg=False):
+        otp = self.set_otp(is_send_msg)
+        if is_send_msg:
             phone = self.phone if phone is None else phone
-            otp = self.set_otp()
             text = f"Your one time password (OTP) for Business Meal is {otp}"
             send_sms(self.phone, text)
 
-    def set_otp(self):
-        otp = get_random_string(length=4, allowed_chars="0123456789")
-        otp = 1234
+    def set_otp(self, is_send_msg=False):
+        otp = (
+            get_random_string(length=4, allowed_chars="0123456789")
+            if is_send_msg
+            else 1234
+        )
         self.verification_code = otp
         self.save()
         return otp
